bef5827ea8aa7d5fcd3c8b843b33a0cf6cb8bd46
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-07-11 15:11:57 +0200

    Remove useless CompilerPass

--- IDCIAssetLoaderBundle.php
+++ IDCIAssetLoaderBundle.php
@@ -7,18 +7,19 @@
 
 namespace IDCI\Bundle\AssetLoaderBundle;
 
-use IDCI\Bundle\AssetLoaderBundle\DependencyInjection\Compiler\AssetLoaderCompilerPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use IDCI\Bundle\AssetLoaderBundle\DependencyInjection\Compiler\AssetProviderCompilerPass;
 
 class IDCIAssetLoaderBundle extends Bundle
 {
+    /**
+     * {@inheritDoc}
+     */
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
 
         $container->addCompilerPass(new AssetProviderCompilerPass());
-        $container->addCompilerPass(new AssetLoaderCompilerPass());
     }
 }
