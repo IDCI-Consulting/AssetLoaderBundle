2f68ade9303e08c03e908d6a0b64cfa99676094c
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2019-07-04 17:40:38 +0200

    Fix: deprecations

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,21 +11,19 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files
+ * This is the class that validates and merges configuration from your app/config files.
  *
  * See {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
  */
 class Configuration implements ConfigurationInterface
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('idci_asset_loader');
-
-        $rootNode
+        $treeBuilder = new TreeBuilder('idci_asset_loader');
+        $treeBuilder->getRootNode()
             ->children()
                 ->arrayNode('providers')
                     ->addDefaultsIfNotSet()
